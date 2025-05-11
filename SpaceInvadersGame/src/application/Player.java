86e56a136f6692342c0d0d1c6510bc4cbd6c9a0c
Andrew Warner <ajwarner005@gmail.com>
2025-05-10 22:36:15 -0400

    add variables for ship speed

--- SpaceInvadersGame/src/application/Player.java
+++ SpaceInvadersGame/src/application/Player.java
@@ -3,8 +3,11 @@ package application;
 import java.io.FileNotFoundException;
 
 public class Player extends Ship{
+	
+	private static double playerWidth = 35;
+	private static int playerSpeed = 3;
 
 	public Player(double startX, double startY) throws FileNotFoundException {
-		super(startX, startY, 3, "./img/playership.png");
+		super(startX, startY, playerWidth, playerSpeed, "./img/playership.png");
 	}
 }
--- SpaceInvadersGame/src/application/Ship.java
+++ SpaceInvadersGame/src/application/Ship.java
@@ -9,14 +9,25 @@ import javafx.scene.image.ImageView;
 public class Ship extends ImageView {
 
 	private double speed;
+	private double width;
+	
+	public double getSpeed() {
+		return speed;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
 	
 	
-	public Ship(double startX, double startY, double speed, String imageURL) throws FileNotFoundException {
+	public Ship(double startX, double startY, double size, double speed, String imageURL) throws FileNotFoundException {
 		super(new Image(new FileInputStream(imageURL)));
 		this.speed = speed;
+		this.width = size;
 		setX(startX);
 		setY(startY);
-		setFitWidth(35);
+		setFitWidth(size);
 		setPreserveRatio(true);
 	}
 	
