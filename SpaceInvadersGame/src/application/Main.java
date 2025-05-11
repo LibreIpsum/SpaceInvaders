c75b2b4e083ef20d0f3c086f76fb98d767008888
Andrew Warner <ajwarner005@gmail.com>
2025-05-10 23:52:56 -0400

    implement collisions

--- SpaceInvadersGame/src/application/Main.java
+++ SpaceInvadersGame/src/application/Main.java
@@ -18,6 +18,8 @@ import javafx.util.Duration;
 
 public class Main extends Application {
 	
+	int appWidth = 800;
+	
 	ArrayList<String> input = new ArrayList<String>();
 	
 	int laserCooldown = 30;
@@ -26,13 +28,17 @@ public class Main extends Application {
 	
 	ArrayList<Enemy> enemies = new ArrayList<Enemy>();
 	int enemySpeed = 1;
+	int currentEnemyStep = 1;
+	int enemySteps = appWidth / enemySpeed;
+	int enemyPad = 50;
+	int enemyWidth = 0;
 	
 	@Override
 	public void start(Stage stage) {
 		try {
 			// main group and scene
 			Group root = new Group();
-			Scene scene = new Scene(root, 800, 800, Color.rgb(0, 0,	15));
+			Scene scene = new Scene(root, appWidth, appWidth, Color.rgb(0, 0,	15));
 			
 			
 			// create a player ship
@@ -40,20 +46,17 @@ public class Main extends Application {
 			root.getChildren().add(player);
 			
 			
-			// create enemies
-			Group enemyBox = new Group();
-			root.getChildren().add(enemyBox);
-			enemyBox.setTranslateX(60);
-			enemyBox.setTranslateY(60);
-			
-			for (int h=0; h<5; h++) {
-				for (int w=0; w<11; w++) {
-					Enemy enemy = new Enemy(w*50, h*50, 1); 	
+			// create enemies			
+			for (int w=0; w<12; w++) {
+				for (int h=0; h<5; h++) {
+					Enemy enemy = new Enemy((w*enemyPad), (h*enemyPad)+60, 1);
 					enemies.add(enemy);
-					enemyBox.getChildren().add(enemy);
-//					System.out.println(h + ", " + w);
+//					enemyBox.getChildren().add(enemy);
+					root.getChildren().add(enemy);
 				}
+				enemyWidth += enemyPad;
 			}
+			enemySteps -= enemyWidth;
 			
 			// event handling
 			scene.setOnKeyPressed(event -> handleKeyPress(event));
@@ -64,7 +67,7 @@ public class Main extends Application {
 			stage.setScene(scene);
 			stage.show();
 			
-			initializeGameLoop(root, player, playerLasers, enemyBox, enemies);
+			initializeGameLoop(root, player, playerLasers, enemies);
 			
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -73,12 +76,12 @@ public class Main extends Application {
 	
 	
 
-	private void initializeGameLoop(Group root, Player player, ArrayList<Laser> lasers, Group enemyBox, ArrayList<Enemy> enemies) {
+	private void initializeGameLoop(Group root, Player player, ArrayList<Laser> lasers, ArrayList<Enemy> enemies) {
         // Set up the game loop with 60 FPS (16.67 milliseconds per frame)
         Duration frameDuration = Duration.millis(16.67);
         KeyFrame gameFrame = new KeyFrame(frameDuration, event -> {
 			try {
-				updateGame(root, player, lasers, enemyBox, enemies);
+				updateGame(root, player, lasers, enemies);
 			} catch (FileNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -91,7 +94,7 @@ public class Main extends Application {
         gameLoop.play();
     }
 
-    private void updateGame(Group root, Player player, ArrayList<Laser> lasers, Group enemyBox, ArrayList<Enemy> enemies) throws FileNotFoundException {
+    private void updateGame(Group root, Player player, ArrayList<Laser> lasers, ArrayList<Enemy> enemies) throws FileNotFoundException {
         // counters
     	cooldownFrame-=1;
     	
@@ -106,7 +109,7 @@ public class Main extends Application {
         // laser firing
         if (input.contains("SPACE")) {
         	if (cooldownFrame <= 0) {
-        		Laser laser = new Laser(player.getX(), player.getY(), 10);
+        		Laser laser = new Laser(player.getX() + (player.getWidth()/2)-5, player.getY(), 10);
     			lasers.add(laser);
     			root.getChildren().add(laser);
     			cooldownFrame = laserCooldown;
@@ -123,17 +126,31 @@ public class Main extends Application {
         		lasers.remove(i);
         		root.getChildren().remove(laser);
         	}
+        	
+        	
         }
         
         
      // Enemy movement
-        enemyBox.setTranslateX(enemyBox.getTranslateX() + enemySpeed);
-        if ((enemyBox.getTranslateX() < 0) || (enemyBox.getTranslateX() + enemyBox.getBoundsInLocal().getWidth() > 800)) {
-        	enemySpeed *= -1;
+        for (int i=0; i<enemies.size(); i++){
+        	Enemy enemy = enemies.get(i);
+        	enemy.setX(enemy.getX() + enemySpeed);
+        	for (int j=0; j<lasers.size();j++) {
+        		Laser laser = lasers.get(j);
+        		if (laser.getBoundsInParent().intersects(enemy.getBoundsInParent())) {
+        			System.out.println("collision" + i + ", " + j);
+        			lasers.remove(j);
+        			root.getChildren().remove(laser);
+        			enemies.remove(i);
+        			root.getChildren().remove(enemy);
+        		}
+        	}
         }
-        
-        Enemy testEnemy = enemies.get(0);
-        System.out.println(testEnemy.localToScene(testEnemy.getX(), testEnemy.getY()).getX());
+        currentEnemyStep += 1;
+        if (currentEnemyStep >= enemySteps) {
+    		enemySpeed *= -1;
+    		currentEnemyStep = 1;
+    	}
     }
     
     
